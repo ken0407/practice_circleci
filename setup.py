1843ef3b60d6ae9f6f661330f1856aa24d2a4943
keeeeen <kentaro.ishii0407@gmail.com>
2020-02-07 01:49:01 +0900

    write description

--- setup.py
+++ setup.py
@@ -12,13 +12,11 @@ with open('LICENSE') as f:
     license = f.read()
 
 setup(
-    name='sample',
+    name='practice_circleci',
     version='0.1.0',
-    description='Sample package for Python-Guide.org',
     long_description=readme,
-    author='Kenneth Reitz',
-    author_email='me@kennethreitz.com',
-    url='https://github.com/kennethreitz/samplemod',
+    author='kentaro ishii',
+    author_email='kentaro.ishii0407@gmail.com',
     license=license,
     packages=find_packages(exclude=('tests', 'docs'))
 )
